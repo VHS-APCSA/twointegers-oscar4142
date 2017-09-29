0bf025dd9efbd15beafc9183ecaab14f0acafbf9
bob <bob@APCSA>
2017-09-29 14:52:17 -0700

    Working on arithmetic method

--- src/ArithmeticClass.java
+++ src/ArithmeticClass.java
@@ -1,16 +1,40 @@
 
 public class ArithmeticClass 
 {
-	{
-		private int two;
-		private int one;
-	}
+	
+		private int bobob;
+		private int ditto;
+	
 	public ArithmeticClass()
 	{
-		this.one = 1;
-		this.two = 2;
+		this.bobob = 1;
+		this.ditto = 2;
 	}
-	
-	
+	public ArithmeticClass(int bobob, int ditto)
+	{
+		this.bobob = bobob;
+		this.ditto = ditto;
+	}
+	public void setBobob(int bobob)
+	{
+		this.bobob = bobob;
+	}
+	public void setDitto(int ditto)
+	{
+		this.ditto = ditto;
+	}
+	public int getBobob()
+	{
+		return bobob;
+	}
+	public int getDitto()
+	{
+		return ditto;
+	}
+	public String arithmetic()
+	{
+		String str = "";
+		return str;
+	}
 }
 	
