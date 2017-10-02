97331842cb4b6cf77b6e875dd9c1b6774189f0d0
bob <bob@APCSA>
2017-10-02 15:13:07 -0700

    Added a runner, finished arithmetic 2.15, working on 2.16

--- src/ArithmeticClass.java
+++ src/ArithmeticClass.java
@@ -1,7 +1,6 @@
 
 public class ArithmeticClass 
 {
-	
 		private int jo;
 		private int jr;
 	
@@ -34,7 +33,33 @@ public class ArithmeticClass
 	public String arithmetic()
 	{
 		String str = "";
-		str += jo + jr;
+		
+		// Adds	'Addition:     1      +     2      =      3'
+		
+		str += "Addition: " + jo + " + " + jr + " = " + (jo + jr) + "\n"; 
+		
+		//Adds	'Subtraction:    1      -      2      =       -1'
+		
+		str += "Subtraction: " + jo + " - " + jr + " = " + (jo - jr) + "\n";
+		
+		//Adds	'Quotient:	   1     /     2      =       0'
+		
+		str += "Quotient: " + jo + " / " + jr + " = " + (jo / jr) + "\n";
+		
+		//Adds	'Product: 	  1     *      2     =       3'
+		
+		str += "Product: " + jo + " * " + jr + " = " + (jo * jr) + "\n";
+		
+		//returns all str
+		return str;
+	}
+	public String comparingIntergers()
+	{
+		String str = "";
+		str += "Comparing Integer:" + "\n";
+		str += "Enter first interger:" + "\n" + jo + "\n";
+		str += "Enter second interger" + "\n" + jr + "\n";
+		str += "Statement is:" + "\n" + (jo <= jr);
 		return str;
 	}
 }
---
+++ src/TwoIntegerRunner.java
@@ -0,0 +1,11 @@
+
+public class TwoIntegerRunner {
+	public static void main(String[] args) 
+	{
+	ArithmeticClass mathy = new ArithmeticClass(5, 3);
+	System.out.println(mathy.arithmetic());
+	ArithmeticClass compare = new ArithmeticClass(3, 3);
+	System.out.println(compare.comparingIntergers());
+	}
+
+}
